c3264a3e70f3ea5c18b0a65f646e56baac282e16
janzaid khaliq <janzaid.ktk98@gmail.com>
2022-09-20 06:11:34 -0700

    pratice

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
+  keypressed: string;
+  input = ' ';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  PressBtn(keypressed: string) {
+    this.input = keypressed;
   }
-
 }
