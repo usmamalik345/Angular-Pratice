ee5b33047f2bf151d9d62bd3a0bff14e57646a3b
janzaid khaliq <janzaid.ktk98@gmail.com>
2022-09-21 05:06:03 -0700

    angular pratice

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,12 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
   keypressed: string;
-  input = ' ';
+  input: any = 0;
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  PressBtn(keypressed: string) {
-    this.input = keypressed;
+  PressBtn(keypressed: any) {
+    this.input = this.input + keypressed;
+    let a = Number(keypressed || 0);
+    let b = Number(keypressed || 0);
+    // this.input = a + b;
+
+    if (keypressed === '=') {
+      this.input = (0, eval)(this.input);
+    } else {
+      this.input += keypressed;
+    }
+    if (keypressed === '=') {
+      let subArray = this.input.split('-');
+      this.input = parseInt(subArray[0]) - parseInt(subArray[1]);
+
+      // this.input = parseInt(this.input) - parseInt(keypressed);
+    } else console.log(typeof keypressed);
+  }
+
+
+    if (keypressed === '=') {
+      let subArray = this.input.split('x');
+      this.input = parseInt(subArray[0]) * parseInt(subArray[1]);
+
+      // this.input = parseInt(this.input) - parseInt(keypressed);
+    } else console.log(typeof keypressed);
+   }
+
+  clear(keypressed: any) {
+    this.input = 0;
   }
 }
