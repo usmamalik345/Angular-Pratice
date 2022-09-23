ba8e9e399670ac71603b15e6020b323aa1794338
janzaid khaliq <janzaid.ktk98@gmail.com>
2022-09-23 02:59:05 -0700

    somechanges

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -17,9 +17,35 @@ export class CartComponent implements OnInit {
   addto(item: string) {
     this.list.push({ id: this.list.length, name: item });
     console.warn(this.list);
-
-   
   }
 
- 
+  get(item: string) {
+    console.log(item);
+  }
+  deletes(item: string) {
+    // this.list.push({ id: this.list.length, name: item });
+    // console.log(item);
+  }
+}
+
+ngOnInit() {
+  this.loadAllTodoList();
+}
+
+loadAllTodoList() {
+  this.todos = this.todoService.getAllTodos();
+}
+
+onClickEditTodoDetail(id) {
+  this.router.navigate(['/todo-detail'], {queryParams:{id:id}});
+}
+
+onClickAddTodo() {
+  this.router.navigate(['/todo-detail']);
+}
+
+onClickTodoDelete(id) {
+  this.todoService.deleteTodoDetail(id);
+  this.loadAllTodoList();
+}
 }
