be8fe1e2a21da918bef405836daedf692da08e7c
Usama Malik <usmamalik344@gmail.com>
2022-10-13 06:13:33 -0700

    Some changes Intodos

--- src/app/Mycommponent/todos/todos.component.ts
+++ src/app/Mycommponent/todos/todos.component.ts
@@ -6,8 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
-  newTask: any;
+  todoList: any[] = [];
+  namey = '';
   constructor() {}
 
   ngOnInit(): void {}
+
+  Addtask(names: string) {
+    this.todoList.push({ names: names });
+
+    // console.warn(this.list);
+  }
+
+  delteTask(indexs: any) {
+    this.todoList.splice(indexs, 1);
+  }
 }
