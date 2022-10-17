b655975c9d1adcda4f7e337f75e5c794d3464a80
Usama Malik <usmamalik344@gmail.com>
2022-10-17 05:33:18 -0700

    learning new

--- src/app/Mycommponent/todo-f/todo-f.component.ts
+++ src/app/Mycommponent/todo-f/todo-f.component.ts
@@ -3,48 +3,60 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo-f',
   templateUrl: './todo-f.component.html',
-  styleUrls: ['./todo-f.component.css']
+  styleUrls: ['./todo-f.component.css'],
 })
 export class TodoFComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  cruntQuestion = 0;
+
+  quizData = [
+    {
+      question: 'Which language runs in a web browser?',
+      a: 'Java',
+      b: 'C',
+      c: 'Python',
+      d: 'JavaScript',
+      correct: 'd',
+    },
+    {
+      question: 'What does CSS stand for?',
+      a: 'Central Style Sheets',
+      b: 'Cascading Style Sheets',
+      c: 'Cascading Simple Sheets',
+      d: 'Cars SUVs Sailboats',
+      correct: 'b',
+    },
+    {
+      question: 'What does HTML stand for?',
+      a: 'Hypertext Markup Language',
+      b: 'Hypertext Markdown Language',
+      c: 'Hyperloop Machine Language',
+      d: 'Helicopters Terminals Motorboats Lamborginis',
+      correct: 'a',
+    },
+    {
+      question: 'What year was JavaScript launched?',
+      a: '1996',
+      b: '1995',
+      c: '1994',
+      d: 'none of the above',
+      correct: 'b',
+    },
+  ];
+
+  next(formvalue: any) {
+    console.log(formvalue);
+
+    if (formvalue.answer === this.quizData[this.cruntQuestion].correct) {
+      this.cruntQuestion=this.cruntQuestion+1
+      alert('pass');
+    } else {
+      alert('error');
+    }
   }
 
-  const quizData = [
-    {
-        question: "Which language runs in a web browser?",
-        a: "Java",
-        b: "C",
-        c: "Python",
-        d: "JavaScript",
-        correct: "d",
-    },
-    {
-        question: "What does CSS stand for?",
-        a: "Central Style Sheets",
-        b: "Cascading Style Sheets",
-        c: "Cascading Simple Sheets",
-        d: "Cars SUVs Sailboats",
-        correct: "b",
-    },
-    {
-        question: "What does HTML stand for?",
-        a: "Hypertext Markup Language",
-        b: "Hypertext Markdown Language",
-        c: "Hyperloop Machine Language",
-        d: "Helicopters Terminals Motorboats Lamborginis",
-        correct: "a",
-    },
-    {
-        question: "What year was JavaScript launched?",
-        a: "1996",
-        b: "1995",
-        c: "1994",
-        d: "none of the above",
-        correct: "b",
-    },
-];
-
+  submitOption() {}
 }
